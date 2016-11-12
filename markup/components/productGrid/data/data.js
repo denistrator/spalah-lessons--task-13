ed4973b1edfb737d4341e7d19907cf18166082aa
denis <denistrator@yandex.ua>
2016-11-12 10:22:06 +0200

    add product grid

---
+++ markup/components/productGrid/data/data.js
@@ -0,0 +1,76 @@
+var data = {
+	products: [
+		{
+			imgName: "img-1",
+			price: "$1.50",
+			title: "FOREVER 21",
+			desc: "Crab Pattern Ankle Socks",
+		},
+		{
+			imgName: "img-1",
+			price: "$1.50",
+			title: "FOREVER 21",
+			desc: "Crab Pattern Ankle Socks",
+		},
+		{
+			imgName: "img-1",
+			price: "$1.50",
+			title: "FOREVER 21",
+			desc: "Crab Pattern Ankle Socks",
+		},
+		{
+			imgName: "img-1",
+			price: "$1.50",
+			title: "FOREVER 21",
+			desc: "Crab Pattern Ankle Socks",
+		},
+		{
+			imgName: "img-1",
+			price: "$1.50",
+			title: "FOREVER 21",
+			desc: "Crab Pattern Ankle Socks",
+		},
+		{
+			imgName: "img-1",
+			price: "$1.50",
+			title: "FOREVER 21",
+			desc: "Crab Pattern Ankle Socks",
+		},
+		{
+			imgName: "img-1",
+			price: "$1.50",
+			title: "FOREVER 21",
+			desc: "Crab Pattern Ankle Socks",
+		},
+		{
+			imgName: "img-1",
+			price: "$1.50",
+			title: "FOREVER 21",
+			desc: "Crab Pattern Ankle Socks",
+		},
+		{
+			imgName: "img-1",
+			price: "$1.50",
+			title: "FOREVER 21",
+			desc: "Crab Pattern Ankle Socks",
+		},
+		{
+			imgName: "img-1",
+			price: "$1.50",
+			title: "FOREVER 21",
+			desc: "Crab Pattern Ankle Socks",
+		},
+		{
+			imgName: "img-1",
+			price: "$1.50",
+			title: "FOREVER 21",
+			desc: "Crab Pattern Ankle Socks",
+		},
+		{
+			imgName: "img-1",
+			price: "$1.50",
+			title: "FOREVER 21",
+			desc: "Crab Pattern Ankle Socks",
+		},
+	]
+};
