3c4a9e706ea3aa33d5ea4c671dc685af7327b44a
denis <denistrator@yandex.ua>
2016-11-12 13:58:29 +0200

    add product recomendation component

---
+++ markup/components/productRecommendation/data/data.js
@@ -0,0 +1,28 @@
+var data = {
+	productsRecommendation: [
+		{
+			imgName: "img-1",
+			price: "$1.50",
+			title: "FOREVER 21",
+			desc: "Abstract Snowflake Ankle Socks"
+		},
+		{
+			imgName: "img-1",
+			price: "$1.99",
+			title: "FOREVER 21",
+			desc: "Abstract Snowflake Ankle Socks"
+		},
+		{
+			imgName: "img-1",
+			price: "$1.99",
+			title: "FOREVER 21",
+			desc: "Abstract Snowflake Ankle Socks"
+		},
+		{
+			imgName: "img-1",
+			price: "$1.50",
+			title: "FOREVER 21",
+			desc: "Abstract Snowflake Ankle Socks"
+		},
+	]
+};
