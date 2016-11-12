fa21df769255a896bc5e7e45cd35dc0293349432
denis <denistrator@yandex.ua>
2016-11-12 19:26:16 +0200

    update built project

--- builds/build_ver-Sat_Nov_12_2016_19_21_12/static/js/maindt9rgm5giowgarqhyqfr.js
+++ builds/build_ver-Sat_Nov_12_2016_19_21_12/static/js/maindt9rgm5giowgarqhyqfr.js
@@ -5,4 +5,31 @@ void 0!==c?null===c?void r.removeAttr(a,b):e&&"set"in e&&void 0!==(d=e.set(a,c,b
 
 
 
+$(function() {
+	var countPlus = $("#input-numbers-plus");
+	var countMinus = $("#input-numbers-minus");
+	var countInput = $("#input-numbers-input");
+
+	var currenVal = countInput.val();
+
+	// fallbacks
+	countInput.attr("type", "text");
+	countInput.removeClass("no-js");
+	countPlus.removeClass("no-js");
+	countMinus.removeClass("no-js");
+
+	countPlus.click(function(){
+		countInput.val(++currenVal);
+	})
+
+	countMinus.click(function(){
+		if(currenVal > 1) countInput.val(--currenVal)
+	})
+})
+
+
+
+
+
+
 
