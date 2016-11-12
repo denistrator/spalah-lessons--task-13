4d1a558f8f5fa417c5629ad4771c673932ae9ea8
denis <denistrator@yandex.ua>
2016-11-12 13:57:45 +0200

    update component

--- markup/components/productGrid/data/data.js
+++ markup/components/productGrid/data/data.js
@@ -1,76 +1,76 @@
 var data = {
-	products: [
+	productsGrid: [
 		{
 			imgName: "img-1",
 			price: "$1.50",
 			title: "FOREVER 21",
-			desc: "Crab Pattern Ankle Socks",
+			desc: "Crab Pattern Ankle Socks"
 		},
 		{
 			imgName: "img-1",
 			price: "$1.50",
 			title: "FOREVER 21",
-			desc: "Crab Pattern Ankle Socks",
+			desc: "Crab Pattern Ankle Socks"
 		},
 		{
 			imgName: "img-1",
 			price: "$1.50",
 			title: "FOREVER 21",
-			desc: "Crab Pattern Ankle Socks",
+			desc: "Crab Pattern Ankle Socks"
 		},
 		{
 			imgName: "img-1",
 			price: "$1.50",
 			title: "FOREVER 21",
-			desc: "Crab Pattern Ankle Socks",
+			desc: "Crab Pattern Ankle Socks"
 		},
 		{
 			imgName: "img-1",
 			price: "$1.50",
 			title: "FOREVER 21",
-			desc: "Crab Pattern Ankle Socks",
+			desc: "Crab Pattern Ankle Socks"
 		},
 		{
 			imgName: "img-1",
 			price: "$1.50",
 			title: "FOREVER 21",
-			desc: "Crab Pattern Ankle Socks",
+			desc: "Crab Pattern Ankle Socks"
 		},
 		{
 			imgName: "img-1",
 			price: "$1.50",
 			title: "FOREVER 21",
-			desc: "Crab Pattern Ankle Socks",
+			desc: "Crab Pattern Ankle Socks"
 		},
 		{
 			imgName: "img-1",
 			price: "$1.50",
 			title: "FOREVER 21",
-			desc: "Crab Pattern Ankle Socks",
+			desc: "Crab Pattern Ankle Socks"
 		},
 		{
 			imgName: "img-1",
 			price: "$1.50",
 			title: "FOREVER 21",
-			desc: "Crab Pattern Ankle Socks",
+			desc: "Crab Pattern Ankle Socks"
 		},
 		{
 			imgName: "img-1",
 			price: "$1.50",
 			title: "FOREVER 21",
-			desc: "Crab Pattern Ankle Socks",
+			desc: "Crab Pattern Ankle Socks"
 		},
 		{
 			imgName: "img-1",
 			price: "$1.50",
 			title: "FOREVER 21",
-			desc: "Crab Pattern Ankle Socks",
+			desc: "Crab Pattern Ankle Socks"
 		},
 		{
 			imgName: "img-1",
 			price: "$1.50",
 			title: "FOREVER 21",
-			desc: "Crab Pattern Ankle Socks",
+			desc: "Crab Pattern Ankle Socks"
 		},
 	]
 };
