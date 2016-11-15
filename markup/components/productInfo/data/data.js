41c52010f5583eaf2d27f335340e167fd64a089e
Andrew <ancam@yandex.ua>
2016-11-15 09:53:35 +0000

    finish product-info

--- markup/components/productInfo/data/data.js
+++ markup/components/productInfo/data/data.js
@@ -2,21 +2,23 @@ var data = {
 	sliderProduct: [
 		{
 			imgName: "sock_yellow",
-			price: "1.50",
-			title: "Lemon-Pattern Ankle Socks",
-			desc: ""
+			
 		},
-		{
-			imgName: "sock_magenta",
-			price: "1.50",
-			title: "Crab Pattern Ankle Socks",
-			desc: "Patterned with a burst of cascading stars and bright crabs, this pair of ankle socks is a perfect antidote to the winter blues (or a great way to give yourself a boost if you're feeling, you know… crabby)."
+		// {
+		// 	imgName: "sock_magenta",
+			
+		// },
+		// {
+		// 	imgName: "sock_blue",
+			
+		// },
+			{
+			imgName: "sock_yellow2",
+			
 		},
-		{
-			imgName: "sock_blue",
-			price: "1.50",
-			title: "Seashell Ankle Socks",
-			desc: "Patterned with a burst of cascading stars and bright crabs, this pair of ankle socks is a perfect antidote to the winter blues (or a great way to give yourself a boost if you're feeling, you know… crabby)."
+			{
+			imgName: "sock_yellow3",
+			
 		},
 	]
 };
--- markup/components/productInfo/productInfo.js
+++ markup/components/productInfo/productInfo.js
@@ -2,5 +2,18 @@ $("#sliderProduct").owlCarousel({
     items: 1,
     dots: false,
     nav: true,
-    loop: true
+    loop: true,
+    navText: ['<svg width="22" height="60"><line x1="22" y1="0" x2="0" y2="30" style="stroke:rgb(0,0,0);stroke-width:1" /> <line x1="0" y1="30" x2="22" y2="60" style="stroke:rgb(0,0,0);stroke-width:1" /></svg>', '<svg width="22" height="60"><line x1="0" y1="0" x2="22" y2="30" style="stroke:rgb(0,0,0);stroke-width:1" /> <line x1="22" y1="30" x2="0" y2="60" style="stroke:rgb(0,0,0);stroke-width:1" /></svg>']
 });
+$('.choose-color-yellow').click(function(){
+     event.preventDefault();
+    $("#sliderProduct").children().css('filter','hue-rotate(0deg)');
+})
+$('.choose-color-pink').click(function(){
+     event.preventDefault();
+    $("#sliderProduct").children().css('filter','hue-rotate(-110deg)');
+})
+$('.choose-color-blue').click(function(){
+     event.preventDefault();
+    $("#sliderProduct").children().css('filter','hue-rotate(130deg)');
+})
