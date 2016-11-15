2bee2c64a8fc8c6e439efea3c986371c2786e230
Andrew <ancam@yandex.ua>
2016-11-15 13:19:11 +0000

    update productInfo, add zoom to images

--- markup/components/productInfo/data/data.js
+++ markup/components/productInfo/data/data.js
@@ -2,23 +2,18 @@ var data = {
 	sliderProduct: [
 		{
 			imgName: "sock_yellow",
-			
+			id: "img-1",
+			imgLarge: "sock_yellow_b"
 		},
-		// {
-		// 	imgName: "sock_magenta",
-			
-		// },
-		// {
-		// 	imgName: "sock_blue",
-			
-		// },
-			{
+		{
 			imgName: "sock_yellow2",
-			
+			id: "img-2",
+			imgLarge: "sock_yellow2_b"
 		},
 			{
 			imgName: "sock_yellow3",
-			
+			id: "img-3",
+			imgLarge: "sock_yellow3_b"
 		},
 	]
 };
