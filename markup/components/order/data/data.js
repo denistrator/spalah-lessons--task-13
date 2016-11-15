57003f6e479d2bd449b6fdbd5d02980a1a9a528d
Andrew <ancam@yandex.ua>
2016-11-15 21:35:18 +0000

    Add work logics of cart, repair script to count sum

--- markup/components/order/data/data.js
+++ markup/components/order/data/data.js
@@ -8,15 +8,33 @@ var data = {
 		},
 		{
 			imgName: "sock_magenta",
-			price: "1.50",
+			price: "1.80",
 			title: "FOREVER 21",
 			desc: "Crab Pattern Ankle Socks"
 		},
 		{
 			imgName: "sock_blue",
-			price: "1.50",
+			price: "2.50",
 			title: "FOREVER 21",
 			desc: "Seashell Ankle Socks"
 		},
+		{
+			imgName: "sock_yellow",
+			price: "2.00",
+			title: "FOREVER 21",
+			desc: "Lemon-Pattern Ankle Socks"
+		},
+		{
+			imgName: "sock_magenta",
+			price: "2.80",
+			title: "FOREVER 21",
+			desc: "Crab Pattern Ankle Socks"
+		},
+		{
+			imgName: "sock_blue",
+			price: "1.90",
+			title: "FOREVER 21",
+			desc: "Seashell Ankle Socks"
+		}
 	]
 };
