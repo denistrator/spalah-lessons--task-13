bfcf01f9b96d7eb1cb1ef6c8c0b4d39bbd36c131
denis <denistrator@yandex.ua>
2016-11-16 16:38:13 +0200

    add titles for pages

--- markup/components/head/data/data.js
+++ markup/components/head/data/data.js
@@ -2,5 +2,29 @@ head: {
     defaults: {
         title: 'default title',
         useSocialMetaTags: true
+    },
+    cart: {
+        title: 'Cart',
+        useSocialMetaTags: true
+    },
+    checkout: {
+        title: 'Checkout',
+        useSocialMetaTags: true
+    },
+    index: {
+        title: 'Home',
+        useSocialMetaTags: true
+    },
+    productPage: {
+        title: 'Super sock',
+        useSocialMetaTags: true
+    },
+    store: {
+        title: 'Store',
+        useSocialMetaTags: true
+    },
+    wishList: {
+        title: 'Wishlist',
+        useSocialMetaTags: true
     }
 }
